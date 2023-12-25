198fb1dcb62ced73c65e4a55629349b61da26560
georgy <palatckingeogy@yandex.ru>
2023-12-25 22:34:55 +0300

    Installed eslint, added the make lint function. Disabled the no-console rule and changed imports/extensions.

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const showName = () => {
-	const userName = readlineSync.question('May I have your name? ');
-	return console.log(`Hello, ${userName}!`);
+const showName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  return console.log(`Hello, ${userName}!`);
 };
 
+export default showName;
