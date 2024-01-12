8eb0e24e501ff25b4fd1bb783d9cee6037a8ce6f
georgy <palatckingeogy@yandex.ru>
2024-01-12 15:23:19 +0300

    Removed imported function. Application launch has been changed.

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import playGame from '../src/index.js';
-import getRandomProgression from '../src/games/progression.js';
+import startProgressionGame from '../src/games/progression.js';
 
-playGame('What number is missing in the progression?', getRandomProgression);
+startProgressionGame();
