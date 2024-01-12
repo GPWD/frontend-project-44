ecf99a33d27a7c0941b803a4c7a2e692ac0205b5
georgy <palatckingeogy@yandex.ru>
2024-01-12 15:22:54 +0300

    Removed imported function. Application launch has been changed.

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import playGame from '../src/index.js';
-import getGreatestCommonDivisor from '../src/games/gcd.js';
+import startGcdGame from '../src/games/gcd.js';
 
-playGame('Find the greatest common divisor of given numbers.', getGreatestCommonDivisor);
+startGcdGame();
