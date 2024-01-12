ffd8e252b9b3ca1197ba1e6fda6c216f74bab797
georgy <palatckingeogy@yandex.ru>
2024-01-12 15:22:12 +0300

    Removed imported function. Application launch has been changed.

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import playGame from '../src/index.js';
-import getRandomExpression from '../src/games/calc.js';
+import startCalcGame from '../src/games/calc.js';
 
-playGame('What is the result of the expression?', getRandomExpression);
+startCalcGame();
