7359f4a171e7a1ea6cf3fb4961fc12f0e9f19762
georgy <palatckingeogy@yandex.ru>
2024-01-12 15:22:38 +0300

    Removed imported function. Application launch has been changed.

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import playGame from '../src/index.js';
-import getOddOrEvenNumber from '../src/games/even.js';
+import startEvenGame from '../src/games/even.js';
 
-playGame('Answer "yes" if the number is even, otherwise answer "no"', getOddOrEvenNumber);
+startEvenGame();
