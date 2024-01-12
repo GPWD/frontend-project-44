8f014e5040bc54dde84d8eea2960a9cc1559c123
georgy <palatckingeogy@yandex.ru>
2024-01-12 15:23:03 +0300

    Removed imported function. Application launch has been changed.

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import playGame from '../src/index.js';
-import primeNumber from '../src/games/prime.js';
+import startPrimeNumberGame from '../src/games/prime.js';
 
-playGame('Answer "yes" if given number is prime. Otherwise answer "no".', primeNumber);
+startPrimeNumberGame();
